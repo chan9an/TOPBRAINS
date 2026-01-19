639128faa19046bc3803f3e1c960b6b8cecd4d32
chan9an <devchandansr@gmail.com>
2026-01-19 10:27:26 +0530

    Adding MultiplicationTable

--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -1,9 +1,26 @@
-﻿class Program
-{
-    public static void Main(string[] args)
-    {
-        
+﻿using System;
 
-        
+class Program
+{
+    static int[] MultiplicationTable(int n, int upto)
+    {
+        int[] result = new int[upto];
+
+        for (int i = 1; i <= upto; i++)
+        {
+            result[i - 1] = n * i;
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        int n = 3;
+        int upto = 5;
+
+        int[] table = MultiplicationTable(n, upto);
+
+        Console.WriteLine(string.Join(", ", table));
     }
 }
